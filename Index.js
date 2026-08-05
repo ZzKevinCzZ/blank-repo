445f803354584b54420a0c09b189cbae06637e87
KevinC <chautrilam2@gmail.com>
2026-08-05 11:56:16 +1000

    Adding addition at the end

--- Index.js
+++ Index.js
@@ -1,2 +1,19 @@
-console.log('Ciao, Mondo!');
-console.log('Hello, Welt!');
+const message1 = 'Ciao, Mondo!';
+const message2 = 'Hello, Welt!';
+
+let a = 5;
+let b = 10;
+
+console.log(message1);
+console.log(message2);
+
+if ((message1) === 'Ciao, Mondo!') {
+    console.log(a);
+}
+
+if ((message2) === 'Hello, Welt!') {
+    console.log(b);
+}
+
+
+console.log('The sum of a and b is: ' + (a + b));
